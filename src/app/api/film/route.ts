9545514236381b838d88ec0d663cccadba3fcf40
khanz1 <assistance.xavier@gmail.com>
2024-06-23 14:35:31 +0700

    fix: unused api

--- src/app/api/film/route.ts
+++
@@ -1,8 +0,0 @@
-import { getFilmList } from "@/features/film/film.action";
-import getFilteredFilms from "@/features/film/film.repository";
-import { FILM_FILTERS } from "@/features/film/types/film.type";
-export const GET = async () => {
-  const filmList = await getFilteredFilms(FILM_FILTERS.POPULAR);
-
-  return Response.json(filmList);
-}
