bc2e990b39975a0cb1e540c9e62ef9a378ad632c
khanz1 <assistance.xavier@gmail.com>
2024-07-14 21:10:51 +0700

    feat: add scheduler to remove redis cache every week

---
+++ src/app/api/cron/route.ts
@@ -0,0 +1,14 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function GET(req: NextRequest) {
+  if (req.headers.get('Authorization') !== `Bearer ${process.env.CRON_SECRET}`) {
+    return NextResponse.json(
+      { message: 'Unauthorized' },
+      {
+        status: 401,
+      },
+    );
+  }
+
+  return NextResponse.json({ ok: true });
+}
